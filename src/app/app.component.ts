b603e947b01ba2e3bfadab3ee8285f9ee430e123
Nikolai <kalistratov@live.com>
2017-11-30 12:15:24 +0100

    Redirect to posts if authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(
+    private _authService:AuthService,
+    private _router: Router
+  ){
+    if(this._authService.isAuthenticated()){
+      this._router.navigate(['/home']);
+    }
+  }
 }
