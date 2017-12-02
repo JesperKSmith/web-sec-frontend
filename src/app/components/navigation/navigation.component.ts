d9b4e5401c922daae5afe0989e52f775c1abcdfa
Nikolai <kalistratov@live.com>
2017-12-02 01:45:44 +0100

    Navigation menu collapse fix

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -12,6 +12,7 @@ import { AlertService } from "../../services/alert-service.service";
 export class NavigationComponent{
   
   loggedInUser: boolean;
+  menuIsVisible: boolean;
 
   constructor(private _authService:AuthService){
      this.loggedInUser = this._authService.isAuthenticated();
@@ -19,6 +20,15 @@ export class NavigationComponent{
 
   logout(): void{
     this._authService.logout();
+    this.collapseMenu();
     location.reload();
+  }
+
+  collapseMenu():void{
+    this.menuIsVisible = false;
+  }
+
+  showMenu():void{
+    this.menuIsVisible = !this.menuIsVisible;
   }
 }
