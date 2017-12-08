a3a03b14f70f380e03d833e5c3dc1fa8c5a20963
Nikolai <kalistratov@live.com>
2017-12-08 11:02:46 +0100

    Changed PostController Api endpoint Url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { environment } from "../environments/environment";
 const apiBaseUrl = environment.apiUrl;
 
 export var Config =  {
-  apiPostsUrl : `${apiBaseUrl}/PostController_v2.php`,
+  apiPostsUrl : `${apiBaseUrl}/PostController.php`,
   apiCommentsUrl : `${apiBaseUrl}/CommentController.php`,
   apiLoginUrl : `${apiBaseUrl}/UserController.php/Login`,
   apiRegisterUrl : `${apiBaseUrl}/UserController.php/Register`,
