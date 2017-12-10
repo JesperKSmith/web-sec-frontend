d2a8a55056c00752a7fb6c52e7fa1f0d5ceda4c8
Nikolai <kalistratov@live.com>
2017-12-10 19:34:24 +0100

    Config file change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,13 @@ const apiBaseUrl = environment.apiUrl;
 
 export var Config =  {
   apiPostsUrl : `${apiBaseUrl}/PostController.php`,
+  apiUserPostsUrl : (user_id:number) => {
+    return `${apiBaseUrl}/PostController.php?user_id=${user_id}`;
+  },
   apiCommentsUrl : `${apiBaseUrl}/CommentController.php`,
   apiLoginUrl : `${apiBaseUrl}/UserController.php/Login`,
   apiRegisterUrl : `${apiBaseUrl}/UserController.php/Register`,
-  devApiPostRequestUrl : "http://localhost:7777/api/test"
+  apiPictureUrl: `${apiBaseUrl}/UserController.php/Picture`,
+  apiAccountPreferencesUrl: `${apiBaseUrl}/PreferenceController.php/User`,
+  apiAccountPreferenceTypesUrl: `${apiBaseUrl}/PreferenceController.php/Types`,
 }
