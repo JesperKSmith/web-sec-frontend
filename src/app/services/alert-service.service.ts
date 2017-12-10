9037139a75787f815f3e1d7f4c799efbdc2ab3f1
Nikolai <kalistratov@live.com>
2017-12-10 19:33:33 +0100

    Alert Service Update

--- src/app/services/alert-service.service.ts
+++ src/app/services/alert-service.service.ts
@@ -4,19 +4,30 @@ declare var swal: any;
 @Injectable()
 export class AlertService {
 
-   //------------------------------------------------------------------
-   success(title:string, message:string): void{
+    //------------------------------------------------------------------
+    success(title: string, message: string): void {
         swal(title, message, 'success');
-   }
-   //------------------------------------------------------------------
+    }
+    //------------------------------------------------------------------
 
-   error(title:string, message:string): void{
+    error(title: string, message: string): void {
         swal(title, message, 'error');
-   }
-   //------------------------------------------------------------------
-   info(title:string): void{
+    }
+    //------------------------------------------------------------------
+    info(title: string): void {
         swal(title);
-   }
-   //------------------------------------------------------------------
+    }
+    //------------------------------------------------------------------
+    alertWithImage(title: string, message: string, image: string): void {
+        swal({
+            title: 'Sweet Success!',
+            text: 'Wow your avatar looks great! \nThe Picture is successfully saved!',
+            imageUrl: image,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Your new amazing Profile Picture',
+            animation: false
+        })
+    }
 
 }
